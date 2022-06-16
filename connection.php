280b6088ab44faa52b7b76242925340e968a711f
Rakesh-sudo-stack <76909034+Rakesh-sudo-stack@users.noreply.github.com>
2022-06-16 14:59:32 +0530

    This is first commit

---
+++ connection.php
@@ -0,0 +1,25 @@
+<?php
+
+$username = 'root';
+$password = '';
+$database = 'meritomaths';
+$server = 'localhost';
+
+$con = mysqli_connect($server,$username,$password);
+$db = mysqli_select_db($con,$database);
+
+if($con){
+    ?>
+    <script>
+        alert("connected");
+    </script>
+    <?php
+}else{
+    ?>
+    <script>
+        alert("not connected");
+    </script>
+    <?php
+}
+
+?>
